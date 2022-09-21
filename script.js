d2a19cc22c3210e0d88f21ed3e2b91d66a403072
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-21 07:13:44 +0300

    const message = document.getElementById('message');

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const form = document.getElementById('form');
 const password1El = document.getElementById('password1');  
 const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
+const message = document.getElementById('message');
